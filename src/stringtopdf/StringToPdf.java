f54901aa173ac81ff730de151455e245ec0be0da
Milos Jelic <milosjelic98@gmail.com>
2023-01-12 07:47:32 +0100

    final

---
+++ src/stringtopdf/StringToPdf.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringtopdf;
+
+/**
+ *
+ * @author milosjelic
+ */
+public class StringToPdf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new Frame().setVisible(true);
+    }
+    
+}
